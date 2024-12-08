cf167c7b97873011e10c823e7c7e5e8924f36423
verdulife <verdu@live.com>
2024-12-08 19:37:40 +0100

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   site: "https://www.lamediterraniajardineria.com",
   integrations: [tailwind(), sitemap()],
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
