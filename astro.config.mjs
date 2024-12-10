0d63dc886b6b3d08d36bf247eeb8ee6b25e3f797
verdulife <verdu@live.com>
2024-12-10 19:36:09 +0100

    remove server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   site: "https://www.lamediterraniajardineria.com",
-  integrations: [tailwind(), sitemap()],
-  output: "server",
-  adapter: vercel()
+  integrations: [tailwind(), sitemap()]
 });
